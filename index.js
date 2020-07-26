74b3d10e00c7293b1773eaa797ae7d6a0f8c8967
Alex18881 <alex18881@gmail.com>
2020-07-26 15:11:50 +0300

    run fix

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
-import express from "express";
+const express = require("express");
 
 const app = express();
 
-app.use('/', express.json, (req,res) => {
+app.get('/', (req, res) => {
+	console.log('Index loading');
+	res.status(200).send('Ok');
+});
+
+app.post('/', express.json, (req,res) => {
 	console.log('req', req);
 	res.status(200).json(req.ips);
-})
+});
 
 app.listen((args) => {
 	console.log(`App started at ${args}`)
-})
+});
