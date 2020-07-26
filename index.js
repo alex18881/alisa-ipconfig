9a69d8c67774970f6efe90c2a24b512c82dec39e
Alex18881 <alex18881@gmail.com>
2020-07-26 16:48:49 +0300

    More update

--- index.js
+++ index.js
@@ -3,16 +3,32 @@ const { request } = require("express");
 
 const app = express();
 
-function makeResponse(request, data) {
+function makeResponse(request, data, endSession) {
 	const { session, version } = request;
 	return {
 		session,
 		version,
 		response: {
 			text: data.toString(),
-			end_session : false
+			end_session: !!endSession
 		}
 	};
+}
+
+function needHelp(request) {
+	const helpWords = ['еще', 'снова', 'заново', 'повтори', 'помощь'];
+	return !request.nlu.tokens || !!request.nlu.tokens.length
+		&& request.nlu.tokens.some((token) => {
+			return stopWords.indexOf(token) !== -1;
+		});
+}
+
+function heedStop(request) {
+	const stopWords = ['хватит', 'спасибо', 'пока', 'хватит'];
+	return request.nlu.tokens && !request.nlu.tokens.length
+		&& request.nlu.tokens.some((token) => {
+			return stopWords.indexOf(token) !== -1;
+		});
 }
 
 app.use(express.json());
@@ -23,8 +39,20 @@ app.get('/', (req, res) => {
 });
 
 app.post('/', (req,res) => {
+	var response = ''
+		endSession = false;
+
+	if (needHelp(req.body.request)) {
+		response = 'Я помгу вам узнать ай пи адрес станции';
+	} else if (heedStop(req.body.request)) {
+		response = 'Пока';
+		endSession = true;
+	} else {
+		response = req.ip;
+	}
+
 	console.log('req', JSON.stringify(req.body), req.ip, req.ips, req.headers);
-	res.status(200).json(makeResponse(req.body, req.ip));
+	res.status(200).json(makeResponse(req.body, response, endSession));
 });
 
 app.listen(process.env.PORT || 8000, (args) => {
